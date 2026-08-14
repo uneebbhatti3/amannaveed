6c600d04783dd44750b7c9232a1c2e975ce80bec
UneebBhatti <uneebbhatti3@gmail.com>
2026-08-14 17:06:57 +0500

    error resolved

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,7 +6,6 @@ import { DATA } from "@/data/resume";
 import Link from "next/link";
 import Markdown from "react-markdown";
 import ContactSection from "@/components/section/contact-section";
-import HackathonsSection from "@/components/section/hackathons-section";
 import ProjectsSection from "@/components/section/projects-section";
 import WorkSection from "@/components/section/work-section";
 import { ArrowUpRight } from "lucide-react";
